ce6f1fc2898433b03fbe084281293d756d350f69
Smruti Ranjan Samal <samalcodes@gmail.com>
2025-01-27 19:59:41 +0530

    My first commit

---
+++ first.js
@@ -0,0 +1,54 @@
+// console.log("hello worlds");
+//  fullname="shiva";
+//  console.log(fullname);
+//  const student = {
+//     fullname:"smruti ranjan samal",
+//     age:23,
+//     cgpa:8.4,
+//     ispass: true,
+// };
+//  console.log(student);
+
+//  const item = {
+//     name:"parker",
+//     ratting:7200,
+//     offer:5,
+//     price:270,
+//  };
+//  console.log(item);
+// let age = prompt("Enter the age 34");
+
+// if (age < 18) {
+//   console.log("Junior");
+// } else if (age > 60) {
+//   console.log("Senior");
+// } else {
+//   console.log("Middle");
+// }
+
+// let price = [250,645,300,900,50];
+// let updatedprice = [];
+// for(let val of price){
+//   updatedprice.push(val-val*0.1);
+// }
+// console.log(updatedprice);
+
+// let companies=["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+// companies.splice(0,1);
+// console.log(companies);
+// companies.splice(2,1,"OLA")
+// console.log(companies);
+// companies.splice(5,0,"Amazon");
+// console.log(companies);
+
+// function countvowels(){
+//   let str = prompt("Enter the string");
+//   let count = 0;
+//   for(let i=0;i<str.length;i++){
+//     if(str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="o" || str[i]=="u"){
+//       count++;
+//     }
+//   }
+//   console.log("count "+count);
+// }
+// countvowels();

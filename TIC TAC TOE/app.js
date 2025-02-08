0c197e24d090558df97d124761c92262a976a877
Smruti Ranjan Samal <samalcodes@gmail.com>
2025-02-08 19:59:30 +0530

    small change

--- TIC TAC TOE/app.js
+++ TIC TAC TOE/app.js
@@ -4,9 +4,9 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true; //playerX, playerO
-let count = 0; //To Track Draw
-// win Patterns.
+let turnO = true;  //playerX, playerO
+let count = 0;    //To Track Draw
+                 // win Patterns.
 const winPatterns = [
   [0, 1, 2],
   [0, 3, 6],
